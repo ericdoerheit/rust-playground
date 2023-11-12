28ae6acba59e74cb95d27b627aac6dca579594b5
Eric Dörheit <eric.doerheit@data-spree.com>
2023-11-12 19:01:56 +0100

    first working version

--- src/datatypes.rs
+++ src/datatypes.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 pub enum DataType {
     // Null(),
     Boolean(bool),
@@ -13,4 +15,26 @@ pub enum DataType {
     Float32(f32),
     Float64(f64),
     Utf8(String),
+}
+
+impl Display for DataType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DataType::Boolean(_) => write!(f, "{}", String::from("Hello")),
+
+            DataType::Int8(value) => write!(f, "{}", value),
+            DataType::Int16(value) => write!(f, "{}", value),
+            DataType::Int32(value) => write!(f, "{}", value),
+            DataType::Int64(value) => write!(f, "{}", value),
+            DataType::UInt8(value) => write!(f, "{}", value),
+            DataType::UInt16(value) => write!(f, "{}", value),
+            DataType::UInt32(value) => write!(f, "{}", value),
+            DataType::UInt64(value) => write!(f, "{}", value),
+            DataType::Float16(value) => write!(f, "{}", value),
+            DataType::Float32(value) => write!(f, "{}", value),
+            DataType::Float64(value) =>write!(f, "{}", value),
+            DataType::Utf8(value) => write!(f, "{}", value),
+        }
+        // write!(f, "{}", String::from("Hello"))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::any::Any;
 use std::fmt::format;
 use std::fs::File;
 use std::ops::Deref;
-use arrow::array::{Array, Int32Array};
+use arrow::array::{Array, Float16Array, Float32Array, Float64Array, Int32Array, Int64Array, UInt16Array, UInt32Array, UInt64Array, UInt8Array};
 use eframe::egui;
 use egui::{FontData, FontDefinitions, FontFamily, FontId, TextStyle};
 use egui_extras::{Column, TableBuilder};
@@ -62,86 +62,11 @@ fn main() -> Result<(), eframe::Error> {
 
     let builder = ParquetRecordBatchReaderBuilder::try_new(file).unwrap();
 
-    println!("Converted arrow schema is: {}", builder.schema());
-
     let fields = builder.schema().fields();
     for field in fields {
-        println!("{}", field);
-        println!("{}", field.data_type());
-
-        let data_type = field.data_type();
-
         let caption = format!("{} [{}]", field.name(), field.data_type());
         table_columns.push(caption);
-
         let col_data = Vec::new();
-
-        // DataType::Float32(50.5);
-
-        // enum SpreadsheetCell {
-        //     Int(i32),
-        //     Float(f64),
-        //     Text(String),
-        // }
-        // let row = vec![
-        //     SpreadsheetCell::Int(3),
-        //     SpreadsheetCell::Text(String::from("blue")),
-        //     SpreadsheetCell::Float(10.12),
-        // ];
-
-        // let col_data = match data_type {
-        //     // ArrowDataType::Null => Vec::<DataType::Null>::new(),
-        //     // ArrowDataType::Boolean => Vec::<bool>::new(),
-        //     // ArrowDataType::Boolean => Vec::<DataType::Boolean>::new(),
-        //     ArrowDataType::Int8 => Vec::<DataType::Int8>::new(),
-        //     ArrowDataType::Int16 => Vec::<DataType::Int16>::new(),
-        //     ArrowDataType::Int32 => Vec::<DataType::Int32>::new(),
-        //     ArrowDataType::Int64 => Vec::<DataType::Int64>::new(),
-        //     ArrowDataType::UInt8 => Vec::<DataType::UInt8>::new(),
-        //     ArrowDataType::UInt16 => Vec::<DataType::UInt16>::new(),
-        //     ArrowDataType::UInt32 => Vec::<DataType::UInt32>::new(),
-        //     ArrowDataType::UInt64 => Vec::<DataType::UInt64>::new(),
-        //     ArrowDataType::Float16 => Vec::<DataType::Float16>::new(),
-        //     ArrowDataType::Float32 => Vec::<DataType::Float32>::new(),
-        //     ArrowDataType::Float64 => Vec::<DataType::Float64>::new(),
-        //     // ArrowDataType::Timestamp(_, _) => Vec::<DataType::Timestamp>::new(),
-        //     // ArrowDataType::Date32 => Vec::<DataType::Date32>::new(),
-        //     // ArrowDataType::Date64 => Vec::<DataType::Date64>::new(),
-        //     // ArrowDataType::Time32(_) => Vec::<DataType::Time32>::new(),
-        //     // ArrowDataType::Time64(_) => Vec::<DataType::Time64>::new(),
-        //     // ArrowDataType::Duration(_) => Vec::<DataType::Duration>::new(),
-        //     // ArrowDataType::Interval(_) => Vec::<DataType::Interval>::new(),
-        //     // ArrowDataType::Binary => Vec::<DataType::Binary>::new(),
-        //     // ArrowDataType::FixedSizeBinary(_) => Vec::<DataType::FixedSizeBinary()>::new(),
-        //     // ArrowDataType::LargeBinary => Vec::<DataType::LargeBinary>::new(),
-        //     ArrowDataType::Utf8 => Vec::<DataType::Utf8>::new(),
-        //     // ArrowDataType::LargeUtf8 => Vec::<DataType::LargeUtf8>::new(),
-        //     // ArrowDataType::List(_) => Vec::<(_)>::new(),
-        //     // ArrowDataType::FixedSizeList(_, _) => Vec::<DataType::FixedSizeList>::new(),
-        //     // ArrowDataType::LargeList(_) => Vec::<DataType::LargeList>::new(),
-        //     // ArrowDataType::Struct(_) => Vec::<DataType::Struct>::new(),
-        //     // ArrowDataType::Union(_, _) => Vec::<DataType::Union>::new(),
-        //     // ArrowDataType::Dictionary(_, _) => Vec::<DataType::Dictionary>::new(),
-        //     // ArrowDataType::Decimal128(_, _) => Vec::<DataType::Decimal128>::new(),
-        //     // ArrowDataType::Decimal256(_, _) => Vec::<DataType::Decimal256>::new(),
-        //     // ArrowDataType::Map(_, _) => Vec::<DataType::Map>::new(),
-        //     // ArrowDataType::RunEndEncoded(_, _) => Vec::<DataType::RunEndEncoded>::new(),
-        //     _ => Vec::<String>::new()
-        // };
-
-
-        /*        match number {
-                    // Match a single value
-                    1 => println!("One!"),
-                    // Match several values
-                    2 | 3 | 5 | 7 | 11 => println!("This is a prime"),
-                    // Match an inclusive range
-                    13..=19 => println!("A teen"),
-                    // Handle the rest of cases
-                    _ => println!("Ain't special"),
-                }*/
-
-        // let col_data = Vec::new();
         table_data.push(col_data);
     }
 
@@ -154,8 +79,6 @@ fn main() -> Result<(), eframe::Error> {
     while let batch = reader.next() {
         match batch {
             Some(b) => {
-                // println!("Read {} records.", r.unwrap().schema());
-                // println!("Read {} records.", b.unwrap().num_rows());
                 let b = b.unwrap();
                 let num_columns = b.num_columns();
                 let columns = b.columns();
@@ -169,22 +92,86 @@ fn main() -> Result<(), eframe::Error> {
                         ArrowDataType::Int8 => {}
                         ArrowDataType::Int16 => {}
                         ArrowDataType::Int32 => {
-                            let int32array = column
+                            let arr = column
                                 .as_any()
                                 .downcast_ref::<Int32Array>()
                                 .expect("Failed to downcast");
-                            for element in int32array {
+                            for element in arr {
                                 table_data_col.push(DataType::Int32(element.unwrap()));
                             }
                         }
-                        ArrowDataType::Int64 => {}
-                        ArrowDataType::UInt8 => {}
-                        ArrowDataType::UInt16 => {}
-                        ArrowDataType::UInt32 => {}
-                        ArrowDataType::UInt64 => {}
-                        ArrowDataType::Float16 => {}
-                        ArrowDataType::Float32 => {}
-                        ArrowDataType::Float64 => {}
+                        ArrowDataType::Int64 => {
+                            let arr = column
+                                .as_any()
+                                .downcast_ref::<Int64Array>()
+                                .expect("Failed to downcast");
+                            for element in arr {
+                                table_data_col.push(DataType::Int64(element.unwrap()));
+                            }
+                        }
+                        ArrowDataType::UInt8 => {
+                            let arr = column
+                                .as_any()
+                                .downcast_ref::<UInt8Array>()
+                                .expect("Failed to downcast");
+                            for element in arr {
+                                table_data_col.push(DataType::UInt8(element.unwrap()));
+                            }
+                        }
+                        ArrowDataType::UInt16 => {
+                            let arr = column
+                                .as_any()
+                                .downcast_ref::<UInt16Array>()
+                                .expect("Failed to downcast");
+                            for element in arr {
+                                table_data_col.push(DataType::UInt16(element.unwrap()));
+                            }
+                        }
+                        ArrowDataType::UInt32 => {
+                            let arr = column
+                                .as_any()
+                                .downcast_ref::<UInt32Array>()
+                                .expect("Failed to downcast");
+                            for element in arr {
+                                table_data_col.push(DataType::UInt32(element.unwrap()));
+                            }
+                        }
+                        ArrowDataType::UInt64 => {
+                            let arr = column
+                                .as_any()
+                                .downcast_ref::<UInt64Array>()
+                                .expect("Failed to downcast");
+                            for element in arr {
+                                table_data_col.push(DataType::UInt64(element.unwrap()));
+                            }
+                        }
+                        ArrowDataType::Float16 => {
+                            let arr = column
+                                .as_any()
+                                .downcast_ref::<Float16Array>()
+                                .expect("Failed to downcast");
+                            for element in arr {
+                                table_data_col.push(DataType::Float16(element.unwrap().to_f32()));
+                            }
+                        }
+                        ArrowDataType::Float32 => {
+                            let arr = column
+                                .as_any()
+                                .downcast_ref::<Float32Array>()
+                                .expect("Failed to downcast");
+                            for element in arr {
+                                table_data_col.push(DataType::Float32(element.unwrap()));
+                            }
+                        }
+                        ArrowDataType::Float64 => {
+                            let arr = column
+                                .as_any()
+                                .downcast_ref::<Float64Array>()
+                                .expect("Failed to downcast");
+                            for element in arr {
+                                table_data_col.push(DataType::Float64(element.unwrap()));
+                            }
+                        }
                         _ => {}
                     }
 
@@ -217,6 +204,7 @@ fn main() -> Result<(), eframe::Error> {
             let mut my_app = Box::<MyApp>::default();
             // my_app.table_data = table_data;
             my_app.table_columns = table_columns;
+            my_app.table_data = table_data;
             my_app
         }),
     )
@@ -239,10 +227,11 @@ impl Default for MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            egui::ScrollArea::both().auto_shrink([false, false]).show(ui, |ui| {
+            // egui::ScrollArea::both().auto_shrink([false, false]).show(ui, |ui| {
                 let mut table = TableBuilder::new(ui)
                     .striped(true)
                     .resizable(true)
+                    .auto_shrink([false, false])
                     .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
                     // .column(Column::auto())
                     // .column(Column::initial(100.0).range(40.0..=300.0))
@@ -264,25 +253,19 @@ impl eframe::App for MyApp {
                     })
                     .body(|mut body| {
                         body.rows(
-                            18.0, self.table_data.len(),
+                            18.0, self.table_data[0].len(),
                             |row_index, mut row| {
-                                row.col(|ui| {
-                                    ui.label(row_index.to_string());
-                                });
-                                row.col(|ui| {
-                                    // ui.label(format!("Data {}", self.table_data[row_index % self.table_data.len()]));
-                                });
-                                row.col(|ui| {
-                                    ui.label("This row has some long text that you may want to clip, or it will take up too much horizontal space!");
-                                });
-                                row.col(|ui| {
-                                    ui.style_mut().wrap = Some(false);
-                                    ui.label("Normal row");
-                                });
+
+                                for i in 0..self.table_columns.len() {
+                                    row.col(|ui| {
+                                        // ui.label(row_index.to_string());
+                                        ui.label(self.table_data[i][row_index].to_string());
+                                    });
+                                }
                             },
                         );
                     });
-            });
+            // });
         });
     }
 }
